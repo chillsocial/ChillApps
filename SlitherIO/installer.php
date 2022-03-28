cfcc838d8ec7a47d822295ba7706e46d51882ff7
Chill <95980826+chillsocial@users.noreply.github.com>
2022-03-28 08:27:03 -0400

    Update installer.php

--- SlitherIO/installer.php
+++ SlitherIO/installer.php
@@ -1,1 +1,14 @@
-
+<?php
+if (!file_exists('../SlitherIO')) {
+    $storePath = '../SlitherIO';
+    mkdir($storePath, 0777, true);
+    $chillRepo = 'https://raw.githubusercontent.com/chillsocial/ChillApps/main/';
+  file_put_contents($storePath.'/preload.txt', file_get_contents($chillRepo.'SlitherIO/source/preload.txt'));
+  file_put_contents($storePath.'/index.php', file_get_contents($chillRepo.'SlitherIO/source/index.php'));
+  header("Location: menu.php?e=App setup complete!");
+  die();
+}else{
+  header("Location: menu.php?e=You already installed this app!");
+  die();
+}
+?>
