270986ed52be92cb8a6cd34cf87913b3068406db
Chill <95980826+chillsocial@users.noreply.github.com>
2022-03-28 08:00:54 -0400

    Update installer.php

--- Store/installer.php
+++ Store/installer.php
@@ -2,11 +2,12 @@
 if (!file_exists('../Store')) {
     $storePath = '../Store';
     mkdir($storePath, 0777, true);
-  file_put_contents($storePath.'/dl.php', file_get_contents(file_get_contents('repo.txt').'Store/source/dl.php'));
-  file_put_contents($storePath.'/index.php', file_get_contents(file_get_contents('repo.txt').'Store/source/index.php'));
-  file_put_contents($storePath.'/menu.php', file_get_contents(file_get_contents('repo.txt').'Store/source/menu.php'));
-  file_put_contents($storePath.'/preload.txt', file_get_contents(file_get_contents('repo.txt').'Store/source/preload.txt'));
-  file_put_contents($storePath.'/setrepo.php', file_get_contents(file_get_contents('repo.txt').'Store/source/setrepo.php'));
+    $chillRepo = 'https://raw.githubusercontent.com/chillsocial/ChillApps/main/';
+  file_put_contents($storePath.'/dl.php', file_get_contents($chillRepo.'Store/source/dl.php'));
+  file_put_contents($storePath.'/index.php', file_get_contents($chillRepo.'Store/source/index.php'));
+  file_put_contents($storePath.'/menu.php', file_get_contents($chillRepo.'Store/source/menu.php'));
+  file_put_contents($storePath.'/preload.txt', file_get_contents($chillRepo.'Store/source/preload.txt'));
+  file_put_contents($storePath.'/setrepo.php', file_get_contents($chillRepo.'Store/source/setrepo.php'));
   header("Location: menu.php?e=App setup complete!");
   die();
 }else{
